644093c9204d5b55b5614cc46c9d5d03972fee79
M Awais Faryad <awaisfaryad25@gmail.com>
2026-08-13 19:11:19 +0500

    text color change

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -8,7 +8,7 @@ const LandingPage = () => {
         {/* <Hero /> */}
         <section className="min-h-[80vh] h-full">
           <div className="flex flex-col items-center justify-center gap-6 min-h-[80vh]">
-            <h1 className="text-4xl font-bold">🚀 Coming Soon</h1>
+            <h1 className="text-4xl font-bold text-[#FFBB5C]">🚀 Coming Soon</h1>
             <h3 className="text-xl font-medium text-gray-500">🔧 Under Development</h3>
             <p className="text-lg text-gray-600">We're building something exceptional. Stay tuned!</p>
           </div>
